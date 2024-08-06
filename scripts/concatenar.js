528b366f7fb2efc33da69dc0dd0ce75693b80e0c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-05 22:51:39 -0600

    strings concatenar

---
+++ scripts/concatenar.js
@@ -0,0 +1,12 @@
+//concatenar arrays
+
+const producto = 'Monitor de 21"';
+const precio = "30 USD ";
+
+console.log(producto);
+console.log(producto.concat(" En descuento"));
+
+console.log(producto + " " + precio);
+
+//template stings plantillas literales con ${``}
+console.log(`El producto ${producto} tiene un precio de ${precio}`);
