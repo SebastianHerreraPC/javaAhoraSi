e47e49d05423085f25cea3d283a4a820c0e073d5
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-05 22:28:33 -0600

    variablñes con var

---
+++ scripts/variables.js
@@ -0,0 +1,10 @@
+// variables con var
+
+// var nombre = "valores"
+var producto = "Monitor de 20 pulgadas";
+console.log(producto);
+//se les puede reasignar el valor despues
+producto = "Monitor de 19 pulgadas";
+
+console.log(typeof producto);
+console.log(producto);
