0ec02707c416e4201cf07bd247077b0e64e746f0
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-05 22:39:27 -0600

    string

---
+++ scripts/strings.js
@@ -0,0 +1,8 @@
+//strings o cadenas de texto representan textos que se pueden asignar a variables
+const producto = 'Monitor de 20"';
+const producto2 = String("monitor 24 pulgadas");
+const producto3 = new String("Monitor de 25 '24' ");
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
