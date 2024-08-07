95bce78f159a1eaef62e918cb5e5d611d8242627
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-06 23:16:25 -0600

    strings split y repeat

---
+++ scripts/convertir.js
@@ -0,0 +1,8 @@
+//Mayusculas y minusculas
+const producto = "Monitor de 21 pulgadas";
+console.log(producto.toUpperCase());
+console.log(producto.toLocaleLowerCase());
+//pasar de numero a string
+const precio = 300;
+console.log(precio);
+console.log(precio.toString());
