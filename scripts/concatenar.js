3dc9248c7119b92597a275426b5ef299c26b0304
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-06 22:57:24 -0600

    strings concatenar sin espacios

--- scripts/concatenar.js
+++ scripts/concatenar.js
@@ -1,7 +1,7 @@
 //concatenar arrays
 
 const producto = 'Monitor de 21"';
-const precio = "30 USD ";
+const precio = "40 USD ";
 
 console.log(producto);
 console.log(producto.concat(" En descuento"));
@@ -10,3 +10,13 @@ console.log(producto + " " + precio);
 
 //template stings plantillas literales con ${``}
 console.log(`El producto ${producto} tiene un precio de ${precio}`);
+
+//¿Que pasa si tiene espacios en blanco?
+
+const producto1 = "        Monitores        ";
+console.log(producto1);
+console.log(producto1.length);
+console.log(producto1.trimStart()); //para eliminar los espacios al inicio
+console.log(producto1.trimEnd()); //para eliminar los espacios al final
+console.log(producto1.trim()); //para eliminar los espacios de inicio y final
+console.log(producto1.trimStart().trimEnd()); //para eliminar los espacios de inicio y final
