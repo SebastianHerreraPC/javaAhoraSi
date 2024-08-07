58bab23b751629404597eb306e5bb62e6c9944cf
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-06 23:53:39 -0600

    operaciones

--- scripts/numeros.js
+++ scripts/numeros.js
@@ -6,3 +6,20 @@ const numero3 = 20.5;
 console.log(numero1);
 console.log(numero2);
 console.log(numero3);
+
+//operaciones basicas
+
+let resultado;
+//sumas
+
+resultado = numero1 + numero2;
+//restas
+resultado = numero1 - numero2;
+//division
+resultado = numero1 / numero2;
+//multiplicacion
+resultado = numero1 * numero2;
+//residuo
+resultado = numero1 % numero2;
+
+console.log(resultado);
