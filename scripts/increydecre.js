3bd73be90bb3b23ec63aac23c0e7fbab719476f6
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-07 23:26:28 -0600

    incrementos y decrementos

---
+++ scripts/increydecre.js
@@ -0,0 +1,9 @@
+//incrementos y decrementos
+// nombreVariable++ incremento
+//nombreVariable-- decremento
+//(si el ++ o -- esta al inicio lo ejecuta de primera
+
+let puntaje = 5;
+
+//para que aumente de 2 en 2 , 3 en 3 , etc se pone nombreVariable +=X X es el numero de lo que va a ir aumentando.
+console.log(puntaje);
