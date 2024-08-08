c4c62c176a42d5d03e03f12763b1771160a1a7a7
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-07 23:30:42 -0600

    string a numero

---
+++ scripts/stringanumero.js
@@ -0,0 +1,14 @@
+//strings a numero
+
+const numero0 = "1";
+const numero1 = "1.5";
+const numero2 = "Uno";
+const numero3 = 1;
+//ya es numero
+console.log(Number.parseInt(numero0));
+console.log(Number.parseFloat(numero1));
+console.log(numero0);
+
+//ver si es entero el numero
+console.log(Number.isInteger(numero3));
+console.log(Number.isInteger(numero1));
