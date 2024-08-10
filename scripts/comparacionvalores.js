6d0b15ed81141a9db2130de84aa3f69076a78943
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-10 17:52:23 -0600

    comparadores

--- scripts/comparacionvalores.js
+++ scripts/comparacionvalores.js
@@ -11,3 +11,15 @@ console.log(numero1 === numero2);
 console.log(numero1 != numero3);
 console.log(numero1 != numero2);
 console.log(numero1 !== numero2);
+
+//comparar null y undefined
+
+let numero;
+
+console.log(numero);
+
+let numero4 = null;
+console.log(numero4);
+
+console.log(numero == numero4);
+console.log(numero === numero4);
