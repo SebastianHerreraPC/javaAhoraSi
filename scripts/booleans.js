f3c9ceeed39dc88b24ae549f07cfb3d14906c5b3
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-10 17:57:40 -0600

    booleans

--- scripts/booleans.js
+++ scripts/booleans.js
@@ -5,3 +5,8 @@ console.log(boolean1);
 console.log(boolean2);
 
 console.log(typeof boolean1);
+
+const boolean3 = new Boolean(true);
+//aqui se creo como objeto
+console.log(boolean3);
+console.log(typeof boolean3);
