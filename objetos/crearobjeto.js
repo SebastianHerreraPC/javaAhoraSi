a1f4ec447b2d5d5c8af32dabd070a71076159cc4
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-10 18:10:55 -0600

    objetos

---
+++ objetos/crearobjeto.js
@@ -0,0 +1,22 @@
+const nombre = "monitor";
+const precio = 300;
+const disponible = true;
+
+// un objeto agrupa todo en una variable se usa las { nombreVariable: valor }
+
+//objeto literal
+const producto = {
+  nombre: 'Monitor de 10"',
+  precio: 200,
+  disponible: true,
+};
+
+console.log(producto);
+//acceder a los valores de mi objeto es con nombreObjeto.nombreVariable o tambien nombreObjeto['nombreVariable]
+
+console.log(producto.nombre);
+console.log(producto["nombre"]);
+
+//para agregar o eliminar propiedades se tiene que hacer asi: nombreObjeto.nuevaVariable = "Valor"(agregar), para eliminar se le pone delete al inicio delete nombreObjeto.nombreVariable
+producto.imagen = "imagen";
+delete producto.disponible;
--- scripts/booleans.js
+++ scripts/booleans.js
@@ -10,3 +10,14 @@ const boolean3 = new Boolean(true);
 //aqui se creo como objeto
 console.log(boolean3);
 console.log(typeof boolean3);
+
+//buenas practicas
+const autenticado = true;
+if (autenticado) {
+  console.log("estas autenticado");
+} else {
+  console.log("no estas ");
+}
+// como el true esta implicito no se pone en el if , si es ternario es ( variable es true o false el ? es como el if y el : es como else )
+
+console.log(autenticado ? "Si estas autenticado" : "no lo estas");
