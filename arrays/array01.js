f7b7f5d3e473fb6cf442f8c45a8d3d8a57194f3c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-11 23:09:41 -0600

    arrays

---
+++ arrays/array01.js
@@ -0,0 +1,9 @@
+//un array sirven para agrupar datos similares a diferencia de un objeto que es una variable que agrupa elementos
+//en lugar de llave y valor este solo tiene valor
+const numeros = [10, 20, 30];
+//no es comun pero pasa
+const meses = new Array("Enero", "Febrero", "Marzo");
+
+console.log(numeros); //3
+
+console.log(meses);
