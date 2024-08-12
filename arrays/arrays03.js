8739217efaa87441e8fe8de310fd98412431ad8d
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-11 23:48:07 -0600

    arrays acceder a elementos

---
+++ arrays/arrays03.js
@@ -0,0 +1,22 @@
+const meses = ["Enero", "Febrero", "Marzo", "Junio", "Julio"];
+
+console.table(meses);
+//conocer cuanto mide el array
+console.log(meses.length);
+
+//recorrer el arreglo con iterador
+
+/*
+for(valor donde va a empezar,cuantas veces se va a iterar,de cuanto en cuanto se va a iterar){
+
+
+}
+
+
+
+
+*/
+
+for (let i = 0; i < meses.length; i++) {
+  console.log(i);
+}
