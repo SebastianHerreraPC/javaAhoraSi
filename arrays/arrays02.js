553870622635fcc15cc623fd3c0ef8f285b08139
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-11 23:16:33 -0600

    arrays acceder a elementos

---
+++ arrays/arrays02.js
@@ -0,0 +1,10 @@
+//como acceder a los elementos de el array
+
+const numeros = [10, 20, 30, 40, 50];
+
+console.log(numeros);
+console.table(numeros);
+
+//para mostrar uno en especifico es
+
+console.log(numeros[3]);
