2474ba310895f51b794a975b035b52d2ac89101c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-16 23:03:35 -0600

    arr

--- arrays/array01.js
+++ arrays/array01.js
@@ -15,5 +15,29 @@ console.log(ejemploCompuesto);
 console.log(ejemploCompuesto.length);
 //solo toma el que esta en ese numero de indice
 console.log(ejemploCompuesto[1]);
-//entrar al array dentro de el array ,
+//entrar al array dentro de el array
 console.log(ejemploCompuesto[2][1]);
+
+//tambien se puede crear el array:
+
+const c = Array.of("X", "Y", "Z", 7, 8, 9);
+console.log(c);
+
+//iniciar un arreglo y asignar el mismo valor a todas las propiedades
+
+const d = Array(100).fill(undefined);
+//agregar elementos
+const colores = ["Rojo", "Verde", "Azul"];
+console.log(colores);
+
+colores.push("Negro");
+console.log(colores);
+//eliminar
+colores.pop();
+console.log(colores);
+
+//for each
+
+colores.forEach(function (elemento, index) {
+  console.log(`<li id="${index}" >${elemento}</li>`);
+});
