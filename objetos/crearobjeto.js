ee972e02dfa5784df38b26027943e5e119e97b70
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-18 21:43:14 -0600

    objetos

--- objetos/crearobjeto.js
+++ objetos/crearobjeto.js
@@ -3,7 +3,9 @@
 const nombre = "monitor";
 const precio = 300;
 const disponible = true;
+//this hace contexto a una función que estamos u
 
+/*Dentro de un boeto las variables son atributos o propiedades y las funciones se les llama metodos*/
 const persona = {
   name: "Sebastian",
   surname: "Herrera",
@@ -13,7 +15,21 @@ const persona = {
   contacto: {
     correo: "correo@ejemplo.com",
   },
+  saludar: function () {
+    console.log("Hola :) ");
+  },
+  decirMiNombre: function () {
+    console.log(
+      `Hola me llamo ${this.name} ${this.surname} y tengo ${this.age}`
+    );
+  },
 };
+console.log(persona);
+console.log(persona.name);
+//invoco la funcion con :
+
+persona.saludar();
+persona.decirMiNombre();
 
 // un objeto agrupa todo en una variable se usa las { nombreVariable: valor }
 
@@ -33,3 +49,11 @@ console.log(producto["nombre"]);
 //para agregar o eliminar propiedades se tiene que hacer asi: nombreObjeto.nuevaVariable = "Valor"(agregar), para eliminar se le pone delete al inicio delete nombreObjeto.nombreVariable
 producto.imagen = "imagen";
 delete producto.disponible;
+
+//para que me de las keys
+
+console.log(Object.keys(persona));
+//para que me de los valores
+console.log(Object.values(persona));
+//para ver si tiene alguna propiedad
+console.log(persona.hasOwnProperty("name"));
