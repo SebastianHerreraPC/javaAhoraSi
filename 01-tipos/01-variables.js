7f9c3ee4ebf84df41e13823c1b633c91b83dccbe
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-19 23:11:07 -0600

    objetos

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,8 @@
+//esta si se puede cambiar
+let nombre = "Hola mundo";
+console.log(nombre);
+nombre = "un Perrito";
+console.log(nombre);
+
+const laConstante = "Soy una constante";
+console.log(laConstante);
---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,6 @@
+let numero = 1;
+let texto = "Esto es un string";
+let booleanos = true;
+let noDefinido;
+let undef = undefined;
+let nulos = null;
