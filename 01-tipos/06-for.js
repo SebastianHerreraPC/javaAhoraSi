8f6aecb2649017a17da0cd3c8126830386f8c760
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-21 22:57:17 -0600

    arrays

---
+++ 01-tipos/06-for.js
@@ -0,0 +1,21 @@
+/**
+ * for es para crear loops pero contiene todo dentro de una sola linea
+ * for(donde inicia; comparacion; exprecion al terminar la iteracion){
+ * }
+ */
+
+// let i = 0;
+// while (i < 10) {
+//   if (i % 2 == 0) {
+//     console.log("numero par", i); //imprime i
+//   }
+//   i++; //aumenta i en uno
+//para irerar de dos en dos , tres en tres, u otro numero debes poner :
+//i=+numeroque se quiere iterar
+// }
+
+for (let i = 2; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log("numero par", i);
+  }
+}
