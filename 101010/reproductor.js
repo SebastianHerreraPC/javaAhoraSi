ff1518134d5c73e73c827601ad2a1abca5e7fa6a
SebastianHerreraPc <sebehepe00@gmail.com>
2024-08-25 22:12:17 -0600

    DOM tripple ten

--- 101010/reproductor.js
+++ 101010/reproductor.js
@@ -3,7 +3,7 @@ let container = document.querySelector(".container");
 //selecciono el contenedor donde se van a agregar las canciones, lo hare mediente el elemento container en lugar de todo el elemento
 let songsContainer = container.querySelector(".songs-container");
 //selecciono todas las canciones de el contenerdor de songsContainer
-let songs = songsContainer.querySelectorAll(".song");
+// let songs = songsContainer.querySelectorAll(".song");
 //selecciono los botones tanto de añadir como de reset
 
 let addButton = container.querySelector(".form__submit-btn_action_add");
@@ -18,10 +18,52 @@ let resetButton = container.querySelector(".form__submit-btn_action_reset");
 //tambien se puede mediante classList
 // nombreVariable.classList.add('nombre de la clase')
 //tambien para cuando ya tengan canciones se les tiene que quitar esta clase, entonces se añade el else
-if (songs.length === 0) {
-  resetButton.setAttribute("disabled", true);
-  resetButton.classList.add("form__submit-btn_disabled");
-} else {
-  resetButton.removeAttribute("disabled");
-  resetButton.classList.remove("form__submit-btn_disabled");
+
+// if (songs.length === 0) {
+//   resetButton.setAttribute("disabled", true);
+//   resetButton.classList.add("form__submit-btn_disabled");
+// } else {
+//   resetButton.removeAttribute("disabled");
+//   resetButton.classList.remove("form__submit-btn_disabled");
+// }
+//el boton de reset sigue apagado y es por que if solo se ejecuta una ves al inicio del codigo, lo que se tiene que hacer para que se ejecute
+//varias veces es simplemente hacerlo una funcion (es la de la linea 22 que esta comentada pero esa se pasa a funcion) para que songs almacene las canciones que se añadan
+//tiene que estar dentro de renderAdded() para que llame a la variable cada vez que se declare la funcion
+
+function renderAdded() {
+  let songs = songsContainer.querySelectorAll(".song");
+  let noSongsElement = container.querySelector(".no-songs");
+
+  if (songs.length === 0) {
+    resetButton.setAttribute("disabled", true);
+    resetButton.style.backgroundColor = "#f1f1f1";
+    noSongsElement.classList.remove("no-songs_hidden");
+  } else {
+    resetButton.removeAttribute("disabled");
+    resetButton.classList.remove("form__submit-btn_disabled");
+    resetButton.style.backgroundColor = "#ffdb4d";
+    noSongsElement.classList.add("no-songs_hidden");
+  }
 }
+
+//innerHTML y textContent
+//añadire un bloque de codigo a el elemento de songs-container para innerHTMl no se usan parentesis sino =
+//aun que se llame varias veces reemplaza el contenido y solo se pone una vez, entonces para hacer que no se eliminen las otras canciones solo se añade un
+//operador de codigo en la guncion, en este caso sera el +=
+function addSong() {
+  songsContainer.innerHTML += `
+    <div class="song">
+    <h4 class="song__artist">The Cars</h4>
+    <p class="song__title">Drive</p>
+    <button class="song__like"></button>
+    </div>`;
+  renderAdded();
+}
+renderAdded();
+
+// EVENTOS Y REACCIONES
+// nombreVariable.addEventListener('lo que hace el usuario', lo que va a pasar o la funcion a la que va a llamar)
+//en este caso aun que se llame a la funcion se le pasa como referencia y no como resultado es decir en lugar de: nombreFuncion() solo poner nombreFuncion;
+
+addButton.addEventListener("click", addSong);
+//la inscripcion de no hay canciones sigue ahi y debe desaparecer cuando ya haya canciones entonces se agrega con el classList.add() o el .remove, ve a la linea 33 de la funcion
--- 101010/ternarios.js
+++ 101010/ternarios.js
@@ -1,2 +1,33 @@
 //el operador ternario es como el if else y se componoe:
 //(condicion)? verdadero: falso
+let edad = 18;
+let eresMayor = edad <= 18 ? "eres mayor" : "eres menor";
+//switch-case sirve cuando tenemos que evaluar diferentes valores para una misma variable
+
+let dia = 2;
+switch (key) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Lunes");
+    break;
+  case 2:
+    console.log("Martes");
+    break;
+  case 3:
+    console.log("Miercoles");
+    break;
+  case 4:
+    console.log("Jueves");
+    break;
+  case 5:
+    console.log("Viernes");
+    break;
+  case 6:
+    console.log("Sabado");
+    break;
+  default:
+    console.log("Opps, se acabaron los dias ");
+    break;
+}
